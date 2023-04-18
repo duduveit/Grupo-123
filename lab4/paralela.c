d81a46d0608ed2c6572b0cd7365c8801bc6eb0db
Ubuntu <eduardovferrao@gmail.com>
2023-04-18 17:27:22 +0000

    Update files

--- lab4/paralela.c
+++ lab4/paralela.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 int **matrizA;
 int *matrizB;
 int *matrizResultado;
+int m, n; //Linha e coluna
+int thread_count; //Número de thread
 
 void imprimirMatriz(int **matriz, int linhas, int colunas) {
   for (int i = 0; i < linhas; i++) {
@@ -18,18 +21,33 @@ void imprimirVetor(int *matriz, int linhas){
     printf("%d\n", matriz[i]);
 }
 
+void *multMatrizes(void * rank){
+  long my_rank = (long) rank;
+  int i, j;
+  int local_m = m/thread_count;
+  int my_first_row = my_rank*local_m; 
+  int my_last_row = (my_rank+1)*local_m-1;
+
+  //printf("Hello from thread %ld of %d\n", my_rank, thread_count);
+
+  for(i = my_first_row; i <= my_last_row; i++){
+    matrizResultado[i] = 0.0;
+    for(j =0; j < n; j++)
+      matrizResultado[i] += matrizA[i][j] * matrizB[j];
+  }
+  return NULL;
+}
+
 int main(int argc, char* argv[]){
-  int m, n;
   int i, j;
 
-  if (argc < 3){
-    printf("Erro: informe as dimensoes da matriz e do vetor.\nExecute o código como ./main linha coluna.\n");
+  if (argc < 4){
+    printf("Erro: informe as dimensoes da matriz e do vetor e o número de threads.\n");
     return 1;
   }
-
   m = atoi(argv[1]); //Númmero de linhas
   n = atoi(argv[2]); //Número de colunas
-
+  
   //Aloca matriz
   matrizA = (int **) calloc(m, sizeof(int *));
   for(i = 0; i < m; i++) matrizA[i] = (int *) calloc(n, sizeof(int));
@@ -42,12 +60,22 @@ int main(int argc, char* argv[]){
   }
   for (i = 0; i < n; i++) matrizB[i] = (i+1)*2;
 
-  //Cálculo da mult de matrizes
-  for (i=0; i < m; i++){
-    for(j=0; j<n; j++){
-      matrizResultado[i] += matrizA[i][j] * matrizB[j];
-    }
-  }
+
+  //Iniciando threads
+  long thread;
+  pthread_t* thread_handles;
+  
+  thread_count = strtol(argv[3], NULL, 10); //Número de threads
+
+  thread_handles = malloc(thread_count * sizeof(pthread_t));
+    
+  for (thread = 0; thread < thread_count; thread++)
+    pthread_create(&thread_handles[thread], NULL, multMatrizes, (void*)thread);
+
+  for (thread = 0; thread < thread_count; thread++)
+    pthread_join(thread_handles[thread], NULL);
+
+  free(thread_handles);
   
   // Imprime resultado
   printf("\nMatriz A\n");
